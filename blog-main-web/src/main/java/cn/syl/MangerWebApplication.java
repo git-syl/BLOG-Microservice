c85baf624272e664aa33a4dae477c3cd47a29235
mmdsyl <510806100@qq.com>
2017-10-30 12:27:01 +0800

    fix package war bugs

--- blog-main-web/src/main/java/cn/syl/MangerWebApplication.java
+++ blog-main-web/src/main/java/cn/syl/MangerWebApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 /**
@@ -15,10 +17,15 @@ import org.springframework.context.annotation.ImportResource;
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @ImportResource(value = "classpath:dubbo/dubbo-consumer.xml")
 @SpringBootApplication
-public class MangerWebApplication {
+public class MangerWebApplication   extends SpringBootServletInitializer {
     public static void main(String args[]){
         SpringApplication application = new SpringApplication(MangerWebApplication.class);
         //application.setBannerMode(Banner.Mode.OFF);
         application.run(args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MangerWebApplication.class);
     }
 }
--- blog-main/blog-main-service/src/main/java/cn/syl/ManagerApplication.java
+++ blog-main/blog-main-service/src/main/java/cn/syl/ManagerApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
@@ -26,7 +28,7 @@ public class ManagerApplication /*extends SpringBootServletInitializer*/ {
 //    @Override
 //    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 //        //tomcat
-//        return builder.sources(Application.class);
+//        return builder.sources(ManagerApplication.class);
 //    }
 
     public static void main(String[] args) throws InterruptedException {
